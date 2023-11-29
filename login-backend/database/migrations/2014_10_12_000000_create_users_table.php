7b69231ee780f3420e343795f9916f50a9a844b0
kmnczr <minczerkristof@gmail.com>
2023-11-29 10:46:30 +0100

    add new user migration file

--- login-backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ login-backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,29 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// Define an anonymous class that extends the Migration class
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
-    }
+    // The 'up' method is called when the migration is run
+    public function up()
+{
+    // Use the Schema facade to create a new 'users' table with the given columns
+    Schema::create('users', function (Blueprint $table) {
+        $table->id();
+        $table->string('email')->unique();
+        $table->string('password');
+        $table->timestamps();
+    });
+}
 };
