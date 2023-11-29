b8210da2ebcb675e3a6a38d05873433aa3ad94ac
kmnczr <minczerkristof@gmail.com>
2023-11-29 10:41:40 +0100

    add routes for the pages

--- login-client/src/App.js
+++ login-client/src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login';  // Import your Login component
+import HomePage from './components/HomePage/HomePage';
+import AddUser from './components/AddUser/AddUser';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<HomePage />}/>
+          <Route path="/addUser" element={<AddUser/>}></Route>
+          {/* Add more routes for other components/pages as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
